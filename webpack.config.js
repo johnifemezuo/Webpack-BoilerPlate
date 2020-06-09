dadd5b0071f78af8cf82a37a443f4106e3cb583d
john ifemezuo <johnifemezuoanayo@gmail.com>
2020-06-09 13:21:25 +0000

    my webpack boiler plate

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-const path =require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "script.js",
-        path: path.resolve(_dirname, 'bundle')
+    entry : './src/index.js',
+    output : {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
-    mode:
+    mode : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
+        ]
+      }
 }
